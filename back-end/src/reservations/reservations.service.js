181ca311961a0d1b8e3ca23f918cf310d9287151
johmert <johmert@users.noreply.github.com>
2022-04-29 10:22:43 -0700

    all us1-be passing

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -4,7 +4,8 @@ const table = "reservations";
 function create(reservation) {
     return knex(table)
         .insert(reservation)
-        .returning("*");
+        .returning("*")
+        .then(created => created[0]);
 }
 
 function list(date) {
